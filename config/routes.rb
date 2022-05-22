6157f3393a9359bf1d2c66f34c12ff7fb0f76461
miyaichi <burnedsoba@gmail.com>
2022-05-22 09:45:42 +0900

    add ユーザー登録時のパラメータにユーザー名を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' },
+             controllers: { registrations: 'users/registrations' }
   root 'users#index'
 end
