2d9343efe9c86656e74793ac23478fbebe76ddd0
miyaichi <burnedsoba@gmail.com>
2022-05-23 23:31:48 +0900

    add トップページを作成/ヘッダーを作成(仮)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' },
              controllers: { registrations: 'users/registrations' }
-  root 'users#index'
+
+  root 'static_pages#top'
 end
