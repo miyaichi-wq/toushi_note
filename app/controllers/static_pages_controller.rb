4e4b46178f082ce1c80e3ae4a55a86e558cdccd8
miyaichi <burnedsoba@gmail.com>
2022-07-07 00:13:16 +0900

    add ログイン時にrootにアクセスするとダッシュボードにリダイレクトするようにした

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,13 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :redirect_to_dashboard
 
   def top
+  end
+
+  private
+
+  def redirect_to_dashboard
+    redirect_to dashboard_path if authenticate_user!
   end
 end
